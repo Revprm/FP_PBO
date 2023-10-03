c711fea2800551a994b2cacf0cd001ac6377bdfd
Revprm <revy.pramana@gmail.com>
2023-10-03 10:09:51 +0700

    feat: added project files

---
+++ Tower Defense/src/Application/Game.java
@@ -0,0 +1,11 @@
+package Application;
+
+import javax.swing.JFrame;
+public class Game extends JFrame{
+    public Game(){
+        setSize(640, 640);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
---
+++ Tower Defense/src/Application/main.java
@@ -0,0 +1,8 @@
+package Application;
+
+public class main {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+}
