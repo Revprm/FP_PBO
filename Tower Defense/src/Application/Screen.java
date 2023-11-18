0b1c7a105045a01629fa7e98d48ab3494425f486
Revprm <revy.pramana@gmail.com>
2023-11-18 17:54:14 +0700

    fix: Display

--- Tower Defense/src/Application/Screen.java
+++ Tower Defense/src/Application/Screen.java
@@ -2,16 +2,28 @@ package Application;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.util.Random;
 
 public class Screen extends JPanel {
+    private Random random;
     public Screen(){
-
+        random = new Random();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        for(int y = 0; y < 20; y++){
+            for(int x = 0; x < 20; x++){
+                g.setColor(getrndcolor());
+                g.fillRect(x*32, y*32, 32, 32);
+            }
+        }
+    }
 
-        g.setColor(Color.green);
-        g.fillRect(0, 0, 640, 640);
+    private Color getrndcolor(){
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return new Color(r, g, b);
     }
 }
