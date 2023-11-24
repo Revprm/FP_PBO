9f6f3d9ed8055125e17b63a5d3b7db94f43e4da8
Ranto Sitorus <rantositorus44@gmail.com>
2023-11-24 14:31:39 +0700

    feat: Keyboard and mouse function, map, tiles

---
+++ Tower Defense/src/inputs/KeyboardListener.java
@@ -0,0 +1,37 @@
+package inputs;
+
+import static Application.GameStates.*;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import Application.GameStates;
+
+public class KeyboardListener implements KeyListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_A)
+			GameStates.gameState = MENU;
+		
+		else if (e.getKeyCode() == KeyEvent.VK_S)
+			GameStates.gameState = PLAYING;
+		
+		else if (e.getKeyCode() == KeyEvent.VK_D)
+			GameStates.gameState = SETTINGS;
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
