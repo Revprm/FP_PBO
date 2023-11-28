0dfd0569b00e4051fa31804e3f7102a3eedca1bb
Ranto Sitorus <rantositorus44@gmail.com>
2023-11-28 20:52:01 +0700

    add: Settings, Quit, and Menu Buttons

--- Tower Defense/src/scenes/Menu.java
+++ Tower Defense/src/scenes/Menu.java
@@ -17,19 +17,27 @@ public class Menu extends GameScene implements SceneMethods {
 
 	private BufferedImage img;
 	private ArrayList<BufferedImage> sprites = new ArrayList<>();
-	private Random random;
+
 	private MyButton bPlaying, bSettings, bQuit;
 
 	public Menu(Game game) {
 		super(game);
-		random = new Random();
 		importImg();
 		loadSprites();
 		initButtons();
 	}
 
 	public void initButtons() {
-		bPlaying = new MyButton("Play", 100, 100, 100, 30);
+		
+		int w = 150;
+		int h = w/3;
+		int x = 640 / 2 - w / 2;
+		int y = 150;
+		int yOffset = 100;
+		
+		bPlaying = new MyButton("Play", x, y, w, h);
+		bSettings = new MyButton("Settings", x, y + yOffset, w, h);
+		bQuit = new MyButton("Quit", x, y + yOffset * 2, w, h);
 	}
 
 	@Override
@@ -39,6 +47,8 @@ public class Menu extends GameScene implements SceneMethods {
 
 	private void drawButtons(Graphics g) {
 		bPlaying.draw(g);
+		bSettings.draw(g);
+		bQuit.draw(g);
 	}
 
 	private void importImg() {
@@ -61,14 +71,16 @@ public class Menu extends GameScene implements SceneMethods {
 
 	}
 
-	private int getRndInt() {
-		return random.nextInt(5);
-	}
-
 	@Override
 	public void mouseClicked(int x, int y) {
 		if (bPlaying.getBounds().contains(x, y)) {
 			SetGameState(PLAYING);
+		}
+		else if (bSettings.getBounds().contains(x, y)) {
+			SetGameState(SETTINGS);
+		}
+		else if (bQuit.getBounds().contains(x, y)) {
+			System.exit(0);
 		}
 
 	}
@@ -76,8 +88,16 @@ public class Menu extends GameScene implements SceneMethods {
 	@Override
 	public void mouseMoved(int x, int y) {
 		bPlaying.setMouseOver(false);
+		bSettings.setMouseOver(false);
+		bQuit.setMouseOver(false);
 		if (bPlaying.getBounds().contains(x, y)) {
 			bPlaying.setMouseOver(true);
+		}
+		else if (bSettings.getBounds().contains(x, y)) {
+			bSettings.setMouseOver(true);
+		}
+		else if (bQuit.getBounds().contains(x, y)) {
+			bQuit.setMouseOver(true);
 		}
 
 	}
@@ -87,18 +107,24 @@ public class Menu extends GameScene implements SceneMethods {
 		if (bPlaying.getBounds().contains(x, y)) {
 			bPlaying.setMousePressed(true);
 		}
+		else if (bSettings.getBounds().contains(x, y)) {
+			bSettings.setMousePressed(true);
+		}
+		else if (bQuit.getBounds().contains(x, y)) {
+			bQuit.setMousePressed(true);
+		}
 
 	}
 
 	@Override
 	public void mouseReleased(int x, int y) {
 		resetButtons();
-		
+
 	}
 
 	private void resetButtons() {
 		bPlaying.resetBooleans();
-		
+
 	}
 
 }
